61b3751dbf72e164dee6700d2e41d855d62cc722
Maximilian Haye <m.haye@tu-berlin.de>
2026-07-06 16:56:19 +0200

    feat: allow direct upgrade from 2025022700 if using forced_plugin_settings
    There have not been any DB schema changes from there to 2025050900, only confirmoverhaul was added. If that is set using forced_plugin_settings, we can safely upgrade despite the early version.

--- db/upgrade.php
+++ db/upgrade.php
@@ -41,20 +41,23 @@ function xmldb_block_coursefeedback_upgrade(int $oldversion): bool {
     $dbman = $DB->get_manager();
 
     // 1) Enforce minimum starting version.
-    if ($oldversion < 2025_05_09_00) {
-        throw new coding_exception(
-            'This upgrade requires plugin version 2025050900 or higher. '
-            . 'Please upgrade to version 2025050900 (git ref f29819d04929e78ff92e836b31784a9d553be82a) before proceeding.'
-        );
+    if ($oldversion < 2025050900) {
+        // Allow the upgrade when confirmoverhaul is set using forced_plugin_settings.
+        if (!($oldversion >= 2025022700 && get_config('block_coursefeedback', 'confirmoverhaul'))) {
+            throw new coding_exception(
+                'This upgrade requires plugin version 2025050900 or higher. '
+                . 'Please upgrade to version 2025050900 (git ref f29819d04929e78ff92e836b31784a9d553be82a) before proceeding.'
+            );
+        }
     }
 
     // 2) Verify that the admin has accepted the major overhaul and possible data loss.
     if ($oldversion < 2025050901) {
         $confirmed = get_config('block_coursefeedback', 'confirmoverhaul');
-        if (empty($confirmed)) {
+        if (!$confirmed) {
             throw new coding_exception(
-                'You must confirm the major overhaul and possible data loss '
-                . 'in the Course Feedback block settings before continuing.'
+                "You must enable 'confirmoverhaul' and accept possible data loss "
+                . 'in the block_coursefeedback plugin settings before continuing.'
             );
         }
 
