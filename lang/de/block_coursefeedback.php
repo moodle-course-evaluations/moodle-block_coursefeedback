ca864986fb1ac740b4e70326710b842f384c442a
Maximilian Haye <m.haye@tu-berlin.de>
2026-07-07 14:01:49 +0200

    fix: prevent saving survey answers when role switched

--- lang/de/block_coursefeedback.php
+++ lang/de/block_coursefeedback.php
@@ -169,6 +169,7 @@ $string['no_answers'] = 'Keine Antworten';
 $string['no_default_survey_period_set'] = 'Der Evaluationszeitraum ist noch nicht gesetzt. Er muss gesetzt werden, bevor Kurse zur Evaluation hinzugefügt werden können.';
 $string['no_edit_survey_period'] = 'Sie sind nicht berechtigt, den Evaluationszeitraum zu ändern.';
 $string['no_evaluation_permissions'] = 'Sie sind in keiner Organisationseinheit berechtigt, Evaluationen oder Fragebögen zu verwalten. Bitte wenden Sie sich an die Evaluationskoordinator/innen oder den Support.';
+$string['no_save_when_switch_role'] = 'Sie dürfen keine Evaluation beantworten, während Sie Ihre Rolle gewechselt haben.';
 $string['no_scale_selected'] = 'Bitten wählen Sie eine Skala';
 $string['no_survey_execution'] = 'Der Kurs {$a->fullname} ist nicht Teil einer vergangenen, laufenden oder zukünftigen Evaluation.';
 $string['not_enough_answers'] = 'Der Bericht kann nicht angezeigt werden, da es nicht genug Antworten gibt.';
